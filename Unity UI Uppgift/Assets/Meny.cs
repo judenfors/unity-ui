fab53bd223536c5f5fc1a17106f090d13b33ee25
judenfors <Jakobsenviktor6@gmail.com>
2023-09-14 14:05:39 +0200

    ahahhaa

---
+++ Unity UI Uppgift/Assets/Meny.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Meny : MonoBehaviour
+{
+    public void startbutton () {
+        SceneManager.LoadScene (1);
+    }
+
+    public void optionsbutton () {
+
+    }
+}
